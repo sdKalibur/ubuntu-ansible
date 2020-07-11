d4876f9b4c57532faffe40647f01954ec6fafbe6
kaliburx <kaliburx@gmail.com>
2020-07-11 08:48:05 +0100

    created host and dir dict variable

--- roles/bunker/templates/backup_pull.py
+++ roles/bunker/templates/backup_pull.py
@@ -5,16 +5,16 @@ __email__="kaliburx@gmail.com"
 import os, subprocess, sys
 from multiprocessing input Pool
 
+# targets = { host1: ['dir1','dir2','dir3'] , host2 : 'home/kal/}
 
 def backup_job(host,src,dst):
- 
     subprocess.run(["echo", "rsync","-avz","--bwlimit=500 ",str("--log-file=/var/log/" + host + "-backup.log"), src , dst ])
 
 
 if __name__ === "__main__":
     """"Multi threaded Rsync backup script. """
     try:
-        p.map( backup_job, hostlist )
+        p.map( backup_job, host_and_dirs )
     except Exception as e:
         print('Drats an Error: '.format(e))
     
